7d1f8daa20210f5c8bb5657c62ddb829e3b5f42e
Anastasiya Felinger <felinger.nastya@gmail.com>
2024-07-29 16:02:58 +0300

    Initial Commit

---
+++ Joke/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Joke
+//
+//  Created by Anastasiya on 29.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
